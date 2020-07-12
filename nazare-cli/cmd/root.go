df6f2abdf1613a1fdc2d1553974e5beead657fff
Gleicon Moraes <gleicon@gmail.com>
2020-07-11 22:12:53 -0300

    flags and default db name

--- nazare-cli/cmd/root.go
+++ nazare-cli/cmd/root.go
@@ -1,24 +1,3 @@
-/*
-Copyright © 2020 Gleicon Moraes <gleicon@gmail.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
@@ -46,22 +25,6 @@ This command line embeds the libraries and provide a way to store and fetch this
 nazare-cli serverless local HLLCounters and Sets
 Use -b </path/to/databasename.db> to persist w/ badgedb (default name nazare.db in the current dir)
 
-HyperLogLog based counters:
-Add to a hll counter: nazare-cli -c -a <countername> <item>
-Estimate counter size: nazare-cli -c -e <countername>
-
-Cuckoo filter based sets:
-Add to a set: nazare-cli -s -a <setname> <item>
-Check if an item belongs to a set: nazare-cli -s -i <setname> <item>
-Remove an item from a set: nazare-cli -s -r <setname> <item>
-Estimate set cardinality: nazare-cli -s -c <setname>
-That's it, no way to get an item from a set, cuckoo filter stores hashes and signal it an item was 'seen'
-
-K/V handling:
-Set a key: nazare-cli -k -s <keyname> <value>
-Get the value of a key: nazare-cli -k -g <keyname>
-Delete a key: nazare-cli -k -d <keyname>
-
 `,
 }
 
@@ -85,7 +48,8 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("database", "b", "nazare.db", "full database path and name. defaults to nazare.db at local dir")
+
 }
 
 // initConfig reads in config file and ENV variables if set.
